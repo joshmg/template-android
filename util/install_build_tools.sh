8eceabd98627757c046d1fc5460c629fcff62614
Josh Green <jgreen@pillartechnology.com>
2014-12-08 14:37:26 -0500

    Fixed a bug in util/install_build_tools.sh.
    ...which caused the script to fail when multiple (hierarchical) gradle files exist.

--- util/install_build_tools.sh
+++ util/install_build_tools.sh
@@ -10,26 +10,66 @@ SDK_PATH="`${api} --get SDK_PATH`/"
 
 DIR=`${api} --get DIR`
 
-build_tools_version=`find "${DIR}" -name "build.gradle" | xargs grep -i '^[ ]*buildToolsVersion.*$' | awk '{ print tolower($0) }' | sed -n 's/^[ ]*buildtoolsversion[ \"]*\([0-9\.]*\)[ \"]*$/\1/p'`
+function list_available_versions() {
+    echo "" 1>&2
+    echo "Obtaining list of available versions... " 1>&2
+    ${SDK_PATH}tools/android list sdk --all | grep -i "build-tools" | awk '{ print tolower($0) }' | sed -n 's/^.*revision[ ]*\([0-9\.]*\).*$/  \1/p' 1>&2
+    echo "" 1>&2
+}
 
-if [ -z "${build_tools_version}" ]; then
-    echo "Cannot automatically detect the Build Tools version." 1>&2
-    echo -n "Build Tools Version: (21.0.2) "
-    read build_tools_version
+function prompt_for_build_tools_version() {
+    list_available_versions
 
-    if [ -z "${build_tools_version}" ]; then
-        build_tools_version='21.0.2'
+    echo -n "Build Tools Version: (21.0.2) " 1>&2
+    read version 
+    echo "${version}"
+}
+
+function get_build_tools_version() {
+    version=`find "${DIR}" -name "build.gradle"  | xargs grep -i '^[ ]*buildToolsVersion.*$' | awk '{ print tolower($0) }' | sed -n 's/^.*buildtoolsversion[ \"]*\([0-9\.]*\)[ \"]*$/\1/p'`
+
+    if [ -z "${version}" ]; then
+        echo "Cannot automatically detect the Build Tools version." 1>&2
+        version=`prompt_for_build_tools_version`
+
+        if [ -z "${version}" ]; then
+            version='21.0.2'
+        fi
+    fi
+
+    echo "${version}"
+}
+
+function get_sdk_package() {
+    version="${1}"
+
+    package_id=`${SDK_PATH}tools/android list sdk --all | grep -i "build-tools.*${version}" | sed -n 's/^[ ]*\([0-9]*\).*/\1/p'`
+
+    if [ -z "${package_id}" ]; then
+        echo "Cannot find Build Tools Version: ${version}" 1>&2
+        echo "" 1>&2
+    else
+        echo "${package_id}"
+    fi
+}
+
+build_tools_version=`get_build_tools_version`
+echo "Searching for SDK package..." 1>&2
+package_id=`get_sdk_package "${build_tools_version}"`
+
+if [ -z "${package_id}" ]; then
+
+    build_tools_version=`prompt_for_build_tools_version`
+
+    package_id=`get_sdk_package "${build_tools_version}"`
+
+    if [ -z "${package_id}" ]; then
+        exit 1
     fi
 fi
 
-echo "Searching for SDK package..."
-package_id=`${SDK_PATH}tools/android list sdk --all | grep -i "build-tools.*${build_tools_version}" | sed -n 's/^[ ]*\([0-9]*\).*/\1/p'`
-
-if [ -z "${package_id}" ]; then
-    echo "Cannot find Build Tools Version: ${build_tools_version}" 1>&2
-    exit 1
-fi
-
-echo "Buid Tools Version: ${build_tools_version} | Package ID: ${package_id}"
+echo "Buid Tools Version: ${build_tools_version} | Package ID: ${package_id}" 1>&2
 
 ${SDK_PATH}tools/android update sdk --no-ui --all --filter "${package_id}"
+
+exit $?
